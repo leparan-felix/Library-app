7671ad0262d2d3cf9a5403ae74c96b208e88f81f
leparan <leparanfelix16@gmail.com>
2025-04-16 22:24:35 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
+  
       <div>
 
-
+        <Navbar/>
        </div>
-    </>
+   
   )
 }
 
