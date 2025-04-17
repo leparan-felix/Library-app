d78db529a0d49cfe8d8863178bd6ab57da198a4b
robinson732 <kimanirobinson336@gmail.com>
2025-04-17 18:57:00 +0300

    initial code

---
+++ BookList.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, use } from 'react';
+import { useParams,useNavigate } from 'react-router-dom';
+const EditBook = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+const [title,setTitle] = useState('');
+const [author,setAuthor] = useState('');
+const [loading,setloading] = useState(true);
+const [error,setError] = useState('null');
+const [book,setBook] = useState({});
+useEffect(() => {
+  fetch(`https://api.example.com/books/${id}`).then((res) => {
+    if (!res.ok) {
+      throw new Error('Book not found');
+      return res.json();      
+    })
+    .catch
