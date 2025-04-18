c4eaa91c9a1d96989d91d9ad9b4010406753337b
robinson732 <kimanirobinson336@gmail.com>
2025-04-18 17:09:57 +0300

    initial real code

--- BookList.jsx
+++ BookList.jsx
@@ -1,17 +1,26 @@
-import React, { useState, useEffect, use } from 'react';
-import { useParams,useNavigate } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import {useParams,useNavigate} from "react-router-dom";
 const EditBook = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
-const [title,setTitle] = useState('');
-const [author,setAuthor] = useState('');
-const [loading,setloading] = useState(true);
-const [error,setError] = useState('null');
-const [book,setBook] = useState({});
-useEffect(() => {
-  fetch(`https://api.example.com/books/${id}`).then((res) => {
-    if (!res.ok) {
-      throw new Error('Book not found');
-      return res.json();      
-    }
-    .catch
+  const {id} = useParams();
+  const {navigate}=useNavigate();
+  const [title,setTitle]=useState("");
+  const [author,setAuthor]=useState("");
+  const [loading,setLoading ]=useState("");
+  const [error,setError]=useState("null");
+
+fetch(`http://localhost:3000/books/${id}`)
+.then((res)=> {if(res.ok)throw new Error("Error fetching data");
+  return res.json();
+}
+).then((data)=>{
+  setTitle(data.title);
+  setAuthor(data.author);
+setLoading(false);
+} ).catch((err)=>{
+  setError(err.message);
+  setLoading(false);
+});
+},[id]);
+const handleSubmit=(e)=>{
+  e.preventDefault(); 
