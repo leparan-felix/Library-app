b5f2c8d2b08d87aa8d1d01ceeda8cf2b774f2824
Lisa Alusa <sayilisaalusa@gmail.com>
2025-04-19 19:47:34 +0300

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,35 @@ import Navbar from './components/Navbar';
 import BookList from './components/BookList';
 import Home from './pages/Home';
 import AddBook from './pages/AddBook';
-
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
+import './css/App.css'; 
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-        <Route path="/add" element={<AddBook />} />
-        <Route path="/books" element={<BookList />} />
-        
-      </Routes>
-    </Router>
+    <div id="root">
+      <Router>
+        <header className="header">
+          <h1>My Personal Book Library</h1>
+          <Navbar />
+          <img 
+            src="https://cdn-icons-png.flaticon.com/512/2232/2232688.png" 
+            alt="Book Library Logo" 
+            className="logo" 
+          />
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add" element={<AddBook />} />
+            <Route path="/books" element={<BookList />} />
+          </Routes>
+        </main>
+
+        <footer className="library-footer">
+          <p>© {new Date().getFullYear()} My Personal Library. All rights reserved.</p>
+        </footer>
+      </Router>
+    </div>
   );
 }
 
